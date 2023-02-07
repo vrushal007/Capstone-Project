417b52e36ae5d36d24a656e2aaf7fe78ee4b366c
vrushal007 <vrushalkpatel007@gmail.com>
2023-02-07 15:20:33 +0530

    added sections in home page
    Signed-off-by: vrushal007 <vrushalkpatel007@gmail.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,7 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import choose1 from '../../images/mainBody/choose-1.png';
 import choose2 from '../../images/mainBody/choose-2.png';
 import choose3 from '../../images/mainBody/choose-3.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -37,7 +38,52 @@ const Home = () => {
                     </div>
                 </section>
             </section>
-            
+            <section className='aboutUs'>
+                <img src={aboutUs} alt="" />
+                <div>
+                    <h1>ABOUT US</h1>
+                    <h2><span style={{ color: 'goldenrod' }}>WE ARE HERE TO DREAM!</span><br />OUR TEAM IS HERE SERVE YOU.</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed molestiae animi soluta, libero aut doloribus ex dolorum facilis deserunt pariatur possimus praesentium eligendi cumque enim at accusantium! Magni fuga adipisci quis sequi ab error, repellendus sit neque eligendi cumque odio.</p>
+                </div>
+            </section>
+            <section className='programs'>
+                <h2>TRAINING <span style={{ color: 'goldenrod' }}>PROGRAMS</span></h2>
+                <div className='programsOption'>
+                    <div>
+                        <Button>YOGA TRAINING SESSION <FontAwesomeIcon style={{ marginLeft: "10px" }} icon={faArrowRight} /></Button>
+                    </div>
+                    <div>
+                        <Button>CARDIO TRAINING SESSION <FontAwesomeIcon style={{ marginLeft: "10px" }} icon={faArrowRight} /></Button>
+                    </div>
+                </div>
+                <Link to='/program' style={{textDecoration:'none'}}><Button style={{ background: 'goldenrod',color:'black', marginTop:"25px",fontSize:"20px",  borderRadius:"0px",padding:'15px' }}>View More</Button></Link>
+            </section>
+            <section className='chooseUs'>
+                <h2><span style={{ color: 'goldenrod' }}>WHY</span> CHOOSE US</h2>
+                <div className='chooseUsOption'>
+                    <div>
+                        <div>
+                            <img src={choose1} alt="" />
+                            <h3>FREE FITNESS TRAINING</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestias quis, magnam nulla architecto facere sint nostrum inventore incidunt provident.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={choose2} alt="" />
+                            <h3>TONS OF CARDIO & STRENGTH</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestias quis, magnam nulla architecto facere sint nostrum inventore incidunt provident.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={choose3} alt="" />
+                            <h3>NO COMMITMENT MEMBERSHIP</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestias quis, magnam nulla architecto facere sint nostrum inventore incidunt provident.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 };
